26b63942a4200c29bde7e03cfc975a780535f53b
Madi <madi_gaziz@mail.ru>
2026-03-03 23:51:31 +0500

    Перенос данных

--- back_end/accounts/admin.py
+++ back_end/accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from accounts.models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ['email']
+    list_display = ['email', 'first_name', 'last_name', 'is_staff', 'is_active']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2', 'is_staff', 'is_active'),
+        }),
+    )
+    search_fields = ['email', 'first_name', 'last_name']
